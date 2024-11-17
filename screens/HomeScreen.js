5975262a499bfa5c0170624573a87d101ad39460
Stef <stef.schalks@gmail.com>
2024-11-17 18:48:04 +0100

    upload2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default HomeScreen = () => {
+    return (
+        <View style={{flex: 1}}>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
